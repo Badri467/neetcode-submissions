d83df0f9b65a1ed8529b720410012cacbb587272
Badrinarayana Kancharla <badrinarayana467@gmail.com>
2026-08-23 00:12:41 +0530

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        while (s.find("()") != string::npos ||
+               s.find("[]") != string::npos ||
+               s.find("{}") != string::npos) {
+
+            if (s.find("()") != string::npos)
+                s.replace(s.find("()"), 2, "");
+
+            if (s.find("[]") != string::npos)
+                s.replace(s.find("[]"), 2, "");
+
+            if (s.find("{}") != string::npos)
+                s.replace(s.find("{}"), 2, "");
+        }
+
+        return s.empty();
+    }
+};
