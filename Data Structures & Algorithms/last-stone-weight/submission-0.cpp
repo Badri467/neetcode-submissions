8e59159486456c34f1903aef1edbacdeecc4ed82
Badrinarayana Kancharla <badrinarayana467@gmail.com>
2026-08-30 11:30:35 +0530

    Add: last-stone-weight - submission-0

---
+++ Data Structures & Algorithms/last-stone-weight/submission-0.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        if(stones.size()==0) return 0;
+        if(stones.size()==1) return stones[0];
+        priority_queue<int>pq;
+        
+        for(int i=0;i<stones.size();i++)
+        pq.push(stones[i]);
+        
+        while(!pq.empty() && pq.size()!=1){
+
+                int a=pq.top();
+                pq.pop();
+                int b=pq.top();
+                pq.pop();
+                if(a!=b){
+                    pq.push(a-b);
+                }
+            
+        }
+        return pq.empty()?0:pq.top();
+    }
+};
